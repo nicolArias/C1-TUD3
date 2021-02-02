8596cc6e952a6dcb6aa4f8b043350b18041bb6fc
Nicol Arias <nicolariaslebro@gmail.com>
2021-02-02 23:51:37 +0100

    Comentaris varias lineas

---
+++ Test4/src/test4/ComentarioVariasLineas.java
@@ -0,0 +1,14 @@
+package test4;
+
+public class ComentarioVariasLineas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<6;/*ahora viene el paso*/ i++)
+            System.out.println(i+" ");
+            System.out.println("FIN DE PROGRAMA");
+
+
+	}
+
+}
