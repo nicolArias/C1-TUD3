2709095420f33d35eae38fcd764ac60e9cee6ff1
Nicol Arias <nicolariaslebro@gmail.com>
2021-02-02 23:49:46 +0100

    Comentario Linea

---
+++ Test3/src/test3/ComentarioLinea.java
@@ -0,0 +1,14 @@
+package test3;
+
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        System.out.println("Adios");
+        System.out.println("FIN DE PROGRAMA");
+
+
+	}
+
+}
